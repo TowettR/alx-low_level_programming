b4d887e149c29a9291a683ee3c7f4e6b00910199
TowettR <reins5595@gmail.com>
2023-03-17 02:02:50 -0700

    scopalamine

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
